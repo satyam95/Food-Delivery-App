9108d1c8f2e1c21271f21cadfa3940bd1c3fa00e
Satyam <satyam@appointy.com>
2023-08-14 22:15:16 +0530

    fix: tailwind config issue

--- src/data/venueCategories.ts
+++ src/data/venueCategories.ts
@@ -56,13 +56,13 @@ export const accordionData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet. Proin sed libero enim sed faucibus turpis in. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Proin libero nunc consequat interdum varius sit. Nec ultrices dui sapien eget mi proin sed libero enim. Nisi scelerisque eu ultrices vitae auctor. Vestibulum lectus mauris ultrices eros in. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Morbi tincidunt augue interdum velit euismod in pellentesque. Id interdum velit laoreet id. Feugiat in fermentum posuere urna nec tincidunt praesent. Urna porttitor rhoncus dolor purus non. Suscipit tellus mauris a diam maecenas sed enim. Mauris pellentesque pulvinar pellentesque habitant morbi tristique.",
   },
   {
-    id: 1,
+    id: 3,
     title: "Top Restaurant Chains",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet. Proin sed libero enim sed faucibus turpis in. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Proin libero nunc consequat interdum varius sit. Nec ultrices dui sapien eget mi proin sed libero enim. Nisi scelerisque eu ultrices vitae auctor. Vestibulum lectus mauris ultrices eros in. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Morbi tincidunt augue interdum velit euismod in pellentesque. Id interdum velit laoreet id. Feugiat in fermentum posuere urna nec tincidunt praesent. Urna porttitor rhoncus dolor purus non. Suscipit tellus mauris a diam maecenas sed enim. Mauris pellentesque pulvinar pellentesque habitant morbi tristique.",
   },
   {
-    id: 1,
+    id: 4,
     title: "Cities We Deliver To",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus at ultrices mi tempus imperdiet. Proin sed libero enim sed faucibus turpis in. Dui nunc mattis enim ut tellus. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Proin libero nunc consequat interdum varius sit. Nec ultrices dui sapien eget mi proin sed libero enim. Nisi scelerisque eu ultrices vitae auctor. Vestibulum lectus mauris ultrices eros in. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Et ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Morbi tincidunt augue interdum velit euismod in pellentesque. Id interdum velit laoreet id. Feugiat in fermentum posuere urna nec tincidunt praesent. Urna porttitor rhoncus dolor purus non. Suscipit tellus mauris a diam maecenas sed enim. Mauris pellentesque pulvinar pellentesque habitant morbi tristique.",
