1b7924713d114e63713c84dc74bd891cdb4cf587
Satyam <satyamsagar30@gmail.com>
2023-10-27 19:19:30 +0530

    fix: build issue

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Exclude test files or directories using a regular expression pattern
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(test|spec)\.tsx$/,
-        use: "ignore-loader",
-      });
-    }
-
-    return config;
-  },
   images: {
     domains: ["b.zmtcdn.com"],
   },
